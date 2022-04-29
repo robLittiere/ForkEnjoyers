682c166834b53c65fddf6accecb7273dfee1c635
Léo <leo.gregori95@gmail.com>
2022-04-29 09:15:27 +0200

    Swipe right and left to change restaurant

--- ForkEnjoyers/UniqueRestaurantViewController.swift
+++ ForkEnjoyers/UniqueRestaurantViewController.swift
@@ -7,6 +7,49 @@
 
 import UIKit
 
+extension CATransition {
+    //New viewController will appear from bottom of screen.
+    func segueFromBottom() -> CATransition {
+        self.duration = 0.375 //set the duration to whatever you'd like.
+        self.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        self.type = CATransitionType.moveIn
+        self.subtype = CATransitionSubtype.fromTop
+        return self
+    }
+    //New viewController will appear from top of screen.
+    func segueFromTop() -> CATransition {
+        self.duration = 0.375 //set the duration to whatever you'd like.
+        self.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        self.type = CATransitionType.moveIn
+        self.subtype = CATransitionSubtype.fromBottom
+        return self
+    }
+     //New viewController will appear from left side of screen.
+    func segueFromLeft() -> CATransition {
+        self.duration = 0.375 //set the duration to whatever you'd like.
+        self.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        self.type = CATransitionType.moveIn
+        self.subtype = CATransitionSubtype.fromLeft
+        return self
+    }
+    //New viewController will pop from right side of screen.
+    func popFromRight() -> CATransition {
+        self.duration = 0.1 //set the duration to whatever you'd like.
+        self.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        self.type = CATransitionType.reveal
+        self.subtype = CATransitionSubtype.fromRight
+        return self
+    }
+    //New viewController will appear from left side of screen.
+    func popFromLeft() -> CATransition {
+        self.duration = 0.1 //set the duration to whatever you'd like.
+        self.timingFunction = CAMediaTimingFunction(name: CAMediaTimingFunctionName.easeInEaseOut)
+        self.type = CATransitionType.reveal
+        self.subtype = CATransitionSubtype.fromLeft
+        return self
+       }
+}
+
 class UniqueRestaurantViewController: UIViewController {
 
     var restaurant: Restaurant!
@@ -100,9 +143,32 @@ class UniqueRestaurantViewController: UIViewController {
         if let swipeGesture = gesture as? UISwipeGestureRecognizer {
                 switch swipeGesture.direction {
                 case .right:
-                    print("Swiped right")
+                    if self.actualIndex == 0 {
+                        self.actualIndex = self.restaurants.count
+                    }
+                    if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "uniquerestaurantviewcontroller") as? UniqueRestaurantViewController {
+                        
+                        vc.restaurant = self.restaurants[self.actualIndex - 1]
+                        vc.restaurants = self.restaurants
+                        vc.actualIndex = self.actualIndex - 1
+                        
+                        let nav = self.navigationController
+                        DispatchQueue.main.async { //make sure all UI updates are on the main thread.
+                                nav?.view.layer.add(CATransition().segueFromLeft(), forKey: nil)
+                                nav?.pushViewController(vc, animated: false)
+                            }
+                    }
                 case .left:
-                    print("Swiped left")
+                    if self.actualIndex == self.restaurants.count - 1 {
+                        self.actualIndex = -1
+                    }
+                    if let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "uniquerestaurantviewcontroller") as? UniqueRestaurantViewController {
+                        
+                        vc.restaurant = self.restaurants[self.actualIndex + 1]
+                        vc.restaurants = self.restaurants
+                        vc.actualIndex = self.actualIndex + 1
+                        self.navigationController?.pushViewController(vc, animated: true)
+                    }
                 default:
                     break
                 }
